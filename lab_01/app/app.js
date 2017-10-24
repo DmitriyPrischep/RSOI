f3773393ca47aee1d1d68ca1592a787aa494ec8e
pdvdev <pdvdev@yandex.ru>
2017-10-24 11:32:13 +0300

    implementation of continuous integration

--- lab_01/app/app.js
+++ lab_01/app/app.js
@@ -1,28 +1,28 @@
-const http = require('http')
-const express = require('express')
-const exphbs  = require('express-handlebars')
-const path = require('path')
+var http = require('http');
+var express = require('express');
+var exphbs  = require('express-handlebars');
+var path = require('path');
 
-const app = express()
-const port = 3000
+var app = express();
+var port = 3000;
 
 app.engine('.hbs', exphbs({
 	defaultLayout: 'main',
 	extname: '.hbs',
 	layoutsDir: path.join(__dirname, 'views/layouts')
-}))
-app.set('view engine', '.hbs')
-app.set('views', path.join(__dirname, 'views'))
+}));
+app.set('view engine', '.hbs');
+app.set('views', path.join(__dirname, 'views'));
 
-app.use(express.static('views/images'))
+app.use(express.static('views/images'));
 
-app.get('/', (request, response) => {
-	response.render('home')
-})
+app.get('/', function (request, response) {
+	response.render('home');
+});
 
-app.listen(port, (err) => {
+app.listen(port, function (err) {
 	if (err) {
-		return console.log('Something bad happened', err)
+		return console.log('Something bad happened', err);
 	}
-	console.log('Server is listening on ' + port)	
-})
+	console.log('Server is listening on ' + port);
+});
